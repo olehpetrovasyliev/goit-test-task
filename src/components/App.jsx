9595522ca98f175538b9f404a122acde57b48929
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-07-28 23:42:26 +0300

    fix: makes pagination work (but still not correctly yet)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,26 @@ import CardsList from './CadsList/CardsList';
 import Card from './Card/Card';
 import { selectUsers } from 'services/redux/selectors';
 import { fetchUsersPageThunk } from 'services/redux/operations';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 export const App = () => {
   const users = useSelector(selectUsers);
   const dispatch = useDispatch();
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
-    dispatch(fetchUsersPageThunk(1));
-  }, [dispatch]);
-
-  return <CardsList arr={users} />;
+    dispatch(fetchUsersPageThunk(page));
+  }, [dispatch, page]);
+  const handleClick = () => {
+    setPage(prev => prev + 1);
+    console.log(page);
+  };
+  return (
+    <>
+      <CardsList arr={users} />;
+      <button type="button" onClick={handleClick}>
+        more users!!!!!
+      </button>
+    </>
+  );
 };
