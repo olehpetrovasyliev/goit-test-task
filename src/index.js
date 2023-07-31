5f823711d9e6718203ebb60018f10ca38a7bdf84
olehpetrovasyliev <olegvasiilyevv@gmail.com>
2023-07-31 17:25:41 +0300

    test: for git. changes basename

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/olehpetrovasyliev/goit-test-task">
+    <BrowserRouter basename="/goit-test-task">
       <Provider store={store}>
         <App />
       </Provider>
