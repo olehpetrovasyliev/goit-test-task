68a8faecbcf935bd3ada1eff78636b3c40417d73
olehpetrovasyliev <128256070+olehpetrovasyliev@users.noreply.github.com>
2023-07-31 13:37:06 +0000

    Deploying to gh-pages from @ olehpetrovasyliev/goit-test-task@5e21e357d2c3c9055f2ebaf9784133a348a93b60 🚀

---
+++ static/js/544.11712fb1.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[544],{544:function(e,r,t){t.r(r);t(791);var h=t(184);r.default=function(){return(0,h.jsx)("body",{children:(0,h.jsx)("h1",{children:"hello from home!"})})}}}]);
+//# sourceMappingURL=544.11712fb1.chunk.js.map
